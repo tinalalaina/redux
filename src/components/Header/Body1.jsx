50f010e5b35fc06a71b03b0121246e7e5b94ae88
tinalalaina <tinalalaina14@gmail.com>
2024-06-15 16:17:52 -1200

    body2

--- src/components/Header/Body1.jsx
+++ src/components/Header/Body1.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import IMG from'../../Photo/deco.png'
+import Body2 from './Body2'
 function Body1() {
   return (
     <div>
         <img class="w3-imagex" src={IMG} alt="deco home" width="1600" height="1060"/>
+   <Body2/>
     </div>
   )
 }
---
+++ src/components/Header/Body2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Body2() {
+  return (
+    <div>Body2</div>
+  )
+}
+
+export default Body2
