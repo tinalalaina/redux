e6a865ddacca3f04fbb9f4215d84a4244e68f744
tinalalaina <tinalalaina14@gmail.com>
2024-06-15 19:05:13 -1200

    front

--- src/components/Header/Body1.jsx
+++ src/components/Header/Body1.jsx
@@ -3,7 +3,7 @@ import IMG from'../../Photo/deco.png'
 import Body2 from './Body2'
 function Body1() {
   return (
-    <div class="container">
+    <div class="">
         <img class="w3-imagex" src={IMG} alt="deco home" width="1600" height="1060"/>
    <Body2/>
     </div>
--- src/components/Header/Body2.jsx
+++ src/components/Header/Body2.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import IMG2 from '../../Photo/table2.png'
 function Body2() {  return (
-   <div className='body2'>
+   <div className='bodyinit container-fluid'>
+    <div className='body2  '>
      <div class="w3-halfi card">
       <img src={IMG2} alt="Concrete meets bricks"/>
      </div>
      <div class="w3-halfi card">
       <img src={IMG2} alt="Concrete meets bricks"/>
      </div>
+     <div class="w3-halfi card">
+      <img src={IMG2} alt="Concrete meets bricks"/>
+     </div>
+     
      <div class="w3-halfi card">
       <img src={IMG2} alt="Concrete meets bricks"/>
      </div>
     </div>
+   </div>
   )
 }
 
