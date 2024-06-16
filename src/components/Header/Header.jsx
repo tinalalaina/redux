c4e4e7fe3220958933ae6b492703955cc928eaaf
tinalalaina <tinalalaina14@gmail.com>
2024-06-15 15:53:10 -1200

    front

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
-
+import './Header.css'
 function Header() {
   return (
-    <div>Header</div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class=" me-auto  mb-lg-0">
+      
+        </div>
+  
+  
+        <div class="links  me-auto "> 
+          <a href="#">About</a>
+          <a href="#">Akjqmkdjfm</a>
+          <a href="#">Aùds</a>
+          <a href="#">jnmt</a>
+          <a href="#">kjhkjt</a>
+          
+         </div>
+         
+        <div class=" mb-lg-0">
+          hello
+        </div>
+  
+  
+      </div>
+      
+      
+    </div>
+  </nav>
   )
 }
 
