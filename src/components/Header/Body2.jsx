58cf6c0db5db7dd80fc928aee4bb83d6736812a9
tinalalaina <tinalalaina14@gmail.com>
2024-06-15 16:45:45 -1200

    front

--- src/components/Header/Body2.jsx
+++ src/components/Header/Body2.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
-function Body2() {
-  return (
-    <div>Body2</div>
+import IMG2 from '../../Photo/table2.png'
+function Body2() {  return (
+   <div className=''>
+     <div class="w3-row-padding">
+    <div class="w3-halfi ">
+      <img src={IMG2} alt="Concrete meets bricks"/>
+    </div>
+    </div>
+    <div class="w3-row-padding">
+    <div class="w3-halfi">
+      <img src={IMG2} alt="Concrete meets bricks"/>
+    </div>
+    </div>
+    <div class="w3-row-padding">
+    <div class="w3-halfi">
+      <img src={IMG2} alt="Concrete meets bricks"/>
+    </div>
+    </div>
+   </div>
   )
 }
 
