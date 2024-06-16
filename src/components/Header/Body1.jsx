aa6a9697dbcafdbf25c92da8460eb956618800c6
tinalalaina <tinalalaina14@gmail.com>
2024-06-16 02:39:43 -1200

    front

--- src/components/Header/Body1.jsx
+++ src/components/Header/Body1.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import IMG from'../../Photo/deco.png'
 import Body2 from './Body2'
+import Service from './Service'
 function Body1() {
   return (
-    <div class="">
+    <div class="container-fluid">
        <div className="image">
          <div className="textebody1 h1">
          Interior Design
       </div>
       </div>
+      <Service/>
    <div className="">
    <Body2/>
    
