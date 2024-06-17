8fb12e95fde6aef0692f88b44eb747f67d594f7d
tinalalaina <tinalalaina14@gmail.com>
2024-06-16 19:15:03 -1200

    front

--- src/components/ProductLists.jsx
+++ src/components/ProductLists.jsx
@@ -1,55 +1,56 @@
-import React from 'react'
-import { useEffect,useState } from 'react';
-import {Link} from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import axios from "axios";
 const ProductLists = () => {
-    const [products, setProducts]= useState([]);
-
-    useEffect(()=>{
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
         getProducts();
-    },[]);
-
-    const getProducts = async()=> {
-        const response = await axios.get('http://localhost:5000/products');
-    setProducts(response.data);
-    }
-    const deleteProduct = async (productId)=>{
-        await axios.delete('http://localhost:5000/products/${productId}');
+    }, []);
+    const getProducts = async () => {
+        const response = await
+            axios.get(`http://localhost:5000/products`);
+        setProducts(response.data);
+    };
+    const deleteProduct = async (productId) => {
+        await
+            axios.delete(`http://localhost:5000/products/${productId}`);
         getProducts();
     }
-  return (
-    <div>
-           <h1 className='title'>Produits</h1>
-        <h2 className='subtitle'>Liste des produits</h2>
-        <Link to="/products/add" className='button is-primary mb-2'>Ajouter un produit</Link>
-        <table className='table is-striped is-fullwidth'>
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nom du produits</th>
-                    <th>Prix</th>
-                    <th>Créer par</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {products.map((product, index)=>(
-  <tr key={product.uuid}>
-  <td>{index + 1}</td>
-  <td>{product.name}</td>
-  <td>{product.price}</td>
-  <td>{product.user.name}</td>
-  <td>
-    <Link to={'/products/edit/${product.uuid}'} className="button is-small is-info">Modifier</Link>
-    <buttom onClick={()=> deleteProduct(product.uuid)} className="button is-small is-danger">Supprimer</buttom>
-   </td>
-</tr>
-                ))}
-              
-            </tbody>
-        </table>
-    </div>
-  )
+    return (
+        <div>
+            <h1 className='title'>Produits</h1>
+            <h2 className='subtitle'>Liste des produits</h2>
+            <Link to="/products/add" className="button is-primary mb-2">Ajouter un nouveaux produit</Link>
+            <table className='table is-striped is-fullwidth'>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nom du produits</th>
+                        <th>Prix</th>
+                        <th>Créer par</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product, index) => (
+                        <tr key={product.uuid}>
+                            <td>{index + 1}</td>
+                            <td>{product.name}</td>
+                            <td>{product.price}</td>
+                            <td>{product.user.name}</td>
+                            <td>
+                                <Link
+                                    to={`/products/edit/${product.uuid}`} className="button is-small is-info">Modifier </Link>
+                                <button className="button is-small 
+is-danger"
+                                    onClick={() => deleteProduct(product.uuid)}>Supprimer
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
-
 export default ProductLists
--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -1,32 +1,56 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { Link } from "react-router-dom";
 const Userlist = () => {
-  return (
-    <div>
-         <h1 className='title'>Utilisateurs</h1>
-        <h2 className='subtitle'>Liste des Utilisateurs</h2>
-        <table className='table is-striped is-fullwidth'>
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nom</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <body>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </body>
-        </table>
-    </div>
-  )
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers();
+    }, []);
+    const getUsers = async () => {
+        const response = await
+            axios.get("http://localhost:5000/users");
+        setUsers(response.data);
+    };
+    const deleteUser = async (userId) => {
+        await
+            axios.delete(`http://localhost:5000/users/${userId}`);
+        getUsers();
+    }
+    return (
+        <div>
+            <h1 className='title'>Utilisateurs</h1>
+            <h2 className='subtitle'>Liste des Utilisateurs</h2>
+            <Link to="/users/add" className="button is-primary 
+mb-2">Ajouter un nouveau Utilisateur</Link>
+            <table className='table is-striped is-fullwidth'>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nom</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((users, index) => (
+                        <tr key={users.uuid}>
+                            <td>{index + 1}</td>
+                            <td>{users.name}</td>
+                            <td>{users.email}</td>
+                            <td>{users.role}</td>
+                            <td>
+                                <Link to={`/users/edit/${users.uuid}`}
+                                    className="button is-small is-info">Modifier </Link>
+                                <button className="button is-small 
+is-danger" onClick={() => deleteUser(users.uuid)}>Supprimer
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
-
 export default Userlist
